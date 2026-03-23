d4c5c7b10808df91dbcddff55306a2cfd7d8732b
Dmitriy Maslov <ximik87@gmail.com>
2026-03-23 08:47:04 +0200

    Clean code

--- src/IcMarkets.BlockchainHistory.Application/Features/Blockchain/Commands/UpdateBlockchainSnapshotCommandHandler.cs
+++ src/IcMarkets.BlockchainHistory.Application/Features/Blockchain/Commands/UpdateBlockchainSnapshotCommandHandler.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using IcMarkets.BlockchainHistory.Application.Abstractions.Persistence;
+﻿using IcMarkets.BlockchainHistory.Application.Abstractions.Persistence;
 using IcMarkets.BlockchainHistory.Domain.Entities;
 using Mediator;
 
